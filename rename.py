bc8a3ef103ed40fa7f2dc45ea52ed1739fdfde18
Dave Webb <github@d5ve.com>
2015-11-18 22:01:43 +1300

    Arg/opt handling working

--- rename.py
+++ rename.py
@@ -3,22 +3,30 @@
 import getopt
 import sys
 
-for arg in sys.argv:
-    print arg
-
 def main(argv):
     doit = False
 
     try:
-        opts, args = getopt.getopt(argv, "hd", ["help", "doit"])
+        opts, args = getopt.gnu_getopt(argv, "hd", ["help", "doit"])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
 
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage("")
+            sys.exit()
+        elif opt in ("-d", "--doit"):
+            doit = True
+        elif opt == "--doit":
+            doit = True
+
+    pattern = args.pop(0)
+
 def usage(msg):
     if msg:
         print msg
-    print "USAGE: rename.py <regex[s]> <files> [--doit]"
- 
+    print "USAGE: rename.py <regex> <files> [--doit]"
+
 if __name__ == "__main__":
     main(sys.argv[1:])
