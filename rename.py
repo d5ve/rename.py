ad745ad215fd510c990d8c00a21b442479a3596c
Dave Webb <github@d5ve.com>
2015-11-21 09:02:01 +1300

    Add docstring and used that in usage()

--- rename.py
+++ rename.py
@@ -1,5 +1,35 @@
 #!/usr/bin/python
 
+"""rename.py - regex batch file renamer
+
+Inspired by Larry Wall's filename fixer perl script:
+        The Perl Coookbook,
+        Chapter 9.9, Renaming Files
+        O'Reilly, 1999
+
+Synopsis:
+    rename.py [options] <pattern> <replacement> [<file1> ... <fileN>]
+
+    Options:
+        --help  This help documentations.
+        --doit  Rename files. The default behaviour is to print what
+                would be done.
+
+Examples:
+
+    # Print what would be done to rename all avi files to just show name and episode string.
+    rename.py '.*(S\d\dE\d\d).*' 'Quantico - \1.avi' *.avi
+
+    # Perform the rename.
+    rename.py '.*(S\d\dE\d\d).*' 'Quantico - \1.avi' *.avi --doit
+
+Licence:
+
+Copyright 2015 Dave Webb
+
+rename.py is free software. You can do *anything* you like with it.
+"""
+
 import getopt
 import re
 import shutil
@@ -11,21 +41,16 @@ def main(argv):
     try:
         opts, args = getopt.gnu_getopt(argv, "hd", ["help", "doit"])
     except getopt.GetoptError:
-        usage()
-        sys.exit(2)
+        usage(getopt.GetoptError)
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             usage("")
-            sys.exit()
         elif opt in ("-d", "--doit"):
-            doit = True
-        elif opt == "--doit":
             doit = True
 
     if len(args) < 3:
-        usage("")
-        sys.exit(2)
+        usage("Please supply <pattern> and <replacement>")
 
     pattern = args.pop(0)
     sub     = args.pop(0)
@@ -42,7 +67,9 @@ def main(argv):
 def usage(msg):
     if msg:
         print msg
-    print "USAGE: rename.py <pattern> <replacement> <files> [--doit]"
+        print
+    print __doc__
+    sys.exit()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
